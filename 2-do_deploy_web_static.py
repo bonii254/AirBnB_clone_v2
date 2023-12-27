cb30c0f4f13260a317636273f9bd406ed87ee8df
bonface murangiri <bonnyrangi95@gmail.com>
2023-12-27 08:02:16 +0000

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,50 @@
 #!/usr/bin/python3
-"""
-Module creates .tgz archive, distributes it to the web servers"""
-
-from fabric.api import local, run, put, env, cd
+"""Module creates .tgz archive, distributes it to the web servers"""
 import os
+from fabric.api import local, cd, put, run, env
 from datetime import datetime
 
 env.hosts = ['18.234.106.42', '34.224.5.203']
 
-
 def do_pack():
-    """a Fabric script that generates a .tgz archive from the
-    contents of the web_static folder"""
+    """Generates a .tgz archive form contents of web static."""
     try:
         local("mkdir -p versions")
         current_time = datetime.now()
-        archive_name = "web_static_{}.tgz".format(
+        archive_filename = "web_static_{}.tgz".format(
             current_time.strftime("%Y%m%d%H%M%S"))
-        result = local("tar -czvf versions/{} web_static".format(archive_name))
-        return "versions/{}".format(archive_name)
+        local("tar -czvf versions/{} web_static".format(archive_filename))
+        return "versions/{}".format(archive_filename)
     except Exception as e:
-        print("Erro:", str(e))
+        print("Error:", str(e))
         return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers,"""
+    """Deploys static files to our web servers.
+
+    Atributes:
+        archive_path(str): path to archive with our static files.
+    Returns:
+        True(boolean): on success,
+        False(boolean): on fail.
+    """
     if not os.path.exists(archive_path):
-        return false
-
-    archive_name = os.path.basename(archive_path)
-    archive_name_without_ext = os.path.splitext((path_name))[0]
-
-    uploaded = put(archive_path, '/tmp/')
+        return False
+	archive_name = os.path.basename(archive_path)
+    archive_name_without_ext = os.path.splitext((archive_name))[0]
+	
+    uploaded = put(archive_path, "/tmp/")
     if uploaded.failed:
         return false
+
     path = run('mkdir -p /data/web_static/releases/{}'.format(
         archive_name_without_ext))
-    path = '/data/web_static/releases/{}'.format(archive_name_without_ext)
-    if path.failed:
+	if path.failed:
         return false
 
-    uncompressed = run('tar -xzvf /temp/{}'.format(archive_name, path))
+    path = '/data/web_static/releases/{}'.format(archive_name_without_ext)
+    uncompressed = run("tar -xzvf /tmp/{} -C {}".format(archive_name, path))
     if uncompressed.failed:
         return false
 
@@ -50,11 +53,11 @@ def do_deploy(archive_path):
     if move.failed:
         return false
 
-    with cd("/data/webstatic"):
+    with cd("/data/web_static"):
         rm_lnk = run("rm -rf /data/web_static/current")
         if rm_lnk.failed:
-            return false
-        update_link = run("ln -sf {} /data/web_static/current".format(path))
+            return False
+        update_lnk = run("ln -sf  {} /data/web_static/current".format(path))
         if update_lnk.failed:
             return False
-    return True
+    return true
